f9bd3378da867ce9c2161873c3d7ccc7497a5f1a
JChungath <jchungath@github.com>
2021-11-26 14:53:23 -0600

    Update

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _db=dbContext;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categorylist = _db.Categories;
+            return View(categorylist);
+        }
+    }
+}
